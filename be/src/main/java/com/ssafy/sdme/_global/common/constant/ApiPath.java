ed2ce81a71c342ba15d7af817c148288de185d84
shs <gustjd6159@naver.com>
2026-03-03 16:32:54 +0900

    chore: 환경 프로필 분리 및 설정 파일 추가

--- be/src/main/java/com/ssafy/sdme/_global/common/constant/ApiPath.java
+++ be/src/main/java/com/ssafy/sdme/_global/common/constant/ApiPath.java
@@ -1,5 +1,5 @@
 package com.ssafy.sdme._global.common.constant;
 
 public class ApiPath {
-    public static final String PATH = "/api/v1";
+    public static final String PATH = "/api";
 }
--- be/src/main/java/com/ssafy/sdme/_global/common/entity/BaseIdEntity.java
+++ be/src/main/java/com/ssafy/sdme/_global/common/entity/BaseIdEntity.java
@@ -2,10 +2,8 @@ package com.ssafy.sdme._global.common.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @MappedSuperclass
 public abstract class BaseIdEntity {
     @Id
