80509305177a4b10189bf6d8daf01c15d6bfbdbc
minkyoung <choimin06@naver.com>
2026-03-17 19:02:24 +0900

    fe/feature: 업체목록조회, 업체상세조회

---
+++ fe/app/vendor/[vendorId]/page.tsx
@@ -0,0 +1,12 @@
+﻿import { VendorDetailPage } from "@/components/views/vendor-detail-page"
+
+export default async function VendorDetailRoute({
+  params,
+}: {
+  params: Promise<{ vendorId: string }>
+}) {
+  const { vendorId } = await params
+
+  return <VendorDetailPage vendorId={vendorId} />
+}
+
---
+++ fe/app/vendor/page.tsx
@@ -0,0 +1,8 @@
+﻿"use client"
+
+import { VendorsView } from "@/components/views/vendors-view"
+
+export default function VendorPage() {
+  return <VendorsView />
+}
+
